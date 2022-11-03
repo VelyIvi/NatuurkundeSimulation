446de428a3b20ad1abb84607d49f0324272db508
Ivi_ <ivan.velychko.me@gmail.com>
2022-11-03 19:09:18 +0100

    More things

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ v = 0 #m/s
 
 m = 1 #kilos
 
+gC = -9.81 
 
+dT = .1 #seconds delta
+
+
+accel = 0
 while h > 0:
-    print("e")
+    print("Height: " , h ,"   Speed: " , v)
+    v = v+accel*dT
+    accel = gC/m
+    h = h + v*dT
