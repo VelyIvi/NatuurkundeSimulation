f9d1c43704f3cc2ab54e829eb19162fc1a9939ba
Ivi_ <ivan.velychko.me@gmail.com>
2022-11-04 14:14:24 +0100

    Finished gravity, now air resistance

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-h = 401000.0 #meters
+Height = 401000.0 #meters
 v = 0.0 #m/s
 
-m = 1.0 #kilos
+mass = 1.0 #kilos
 
-gC = 9.81
 
 time = 0.0
 dT = .1 #seconds delta
 
 
+massEarth = 5.972 * pow(10, 24)
+gravityConstant = 6.67430* pow(10, -11)
+earthRad = 6371000
+def CalcGrav():
+    force = gravityConstant * ((massEarth*mass)/(pow(Height+earthRad, 2)))
+    return force
+
 accel = 0
-while h > 0:
+while Height > 0:
     time += dT
     v = v+accel*dT
-    accel = gC/m
-    h = h - v*dT
-    print("Height: " , round(h, 4) ,"   Speed: " , round(v, 4), "   Time: ", round(time, 4))
+    Grav = CalcGrav()
+    accel = Grav/mass
+    Height = Height - v*dT
+    print("Height: " , round(Height, 4) ,"   Speed: " , round(v, 4), "   Time: ", round(time, 4), " Gravity: ", round(Grav, 4))
+
+
+
