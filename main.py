41674667bff574ac541265a4cc54a8a407e1ce40
Ivi_ <ivan.velychko.me@gmail.com>
2022-11-04 12:21:10 +0100

    Meer toegevoegd

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-h = 100.0 #meters
-v = 0 #m/s
+h = 401000.0 #meters
+v = 0.0 #m/s
 
-m = 1 #kilos
+m = 1.0 #kilos
 
-gC = -9.81 
+gC = 9.81
 
+time = 0.0
 dT = .1 #seconds delta
 
 
 accel = 0
 while h > 0:
-    print("Height: " , h ,"   Speed: " , v)
+    time += dT
     v = v+accel*dT
     accel = gC/m
-    h = h + v*dT
+    h = h - v*dT
+    print("Height: " , round(h, 4) ,"   Speed: " , round(v, 4), "   Time: ", round(time, 4))
