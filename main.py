b52099cc36a089dad5223a626642e84cedde18dc
Ivi_ <ivan.velychko.me@gmail.com>
2022-11-08 11:28:13 +0100

    I think air resistance done. Without air density

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ areaMove = (4*PI*ballRadius*ballRadius)/2 #m^2
 DragCoef = 0.47
 
 time = 0.0
-dT = 1 #seconds delta
+dT = 10 #seconds delta
 
 massEarth = 5.972 * pow(10, 24)
 gravityConstant = 6.67430* pow(10, -11)
@@ -36,9 +36,10 @@ def calcAirRes():
 accel = 0
 while Height > 0:
     time += dT
-    v = v+accel*dT
-    Grav = calcGrav()
     Air = calcAirRes()
+    Grav = calcGrav()
+
+    v = v+accel*dT
     accel = (Grav-Air)/mass
     Height = Height - v*dT
     print("Height: " , round(Height, 4) ,"   Speed: " , round(v, 4), "   Time: ", round(time, 4), " Gravity: ", round(Grav, 4), " AirRes: ", round(Air, 4))
