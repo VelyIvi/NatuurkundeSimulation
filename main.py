acb109b531b1d29142e8db95b8a9d72b24f5f3df
Ivi_ <ivan.velychko.me@gmail.com>
2022-11-10 10:27:26 +0100

    Almost air resistance done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
+
 PI = 3.14159265359
 
-Height = 401000.0 #meters
+height = 401000.0 #meters
 v = 0.0 #m/s
 
 mass = 1.0 #kilos
@@ -16,13 +18,30 @@ gravityConstant = 6.67430* pow(10, -11)
 earthRad = 6371000
 
 
+timeList = []
+heightList = []
+speedList = []
+accelList = []
+airList = []
+
 def calcGrav():
-    force = gravityConstant * ((massEarth*mass)/(pow(Height+earthRad, 2)))
+    force = gravityConstant * ((massEarth*mass)/(pow(height+earthRad, 2)))
     return force
 
-
+#https://en.wikipedia.org/wiki/Density_of_air
+SLSAPressure = 101325
+SLSTemperature = 288.15
+ESGAcceleration = 9.80665
+LRTemperature = 0.0065
+IGConstant = 8.31446
+MolarMass = 0.0289652
+def calcAirPressure():
+    press = SLSAPressure*pow((1-(LRTemperature*height)/(SLSTemperature)), ((ESGAcceleration*MolarMass)/(IGConstant*LRTemperature)))
+    return press
 def calcAirDensity():
-    return 1.204
+    press = calcAirPressure()
+    density = (press*MolarMass)/(IGConstant*(SLSTemperature-LRTemperature*height))
+    return density
 
 def calcAirConst():
     density = calcAirDensity()
@@ -34,12 +53,25 @@ def calcAirRes():
     return calcAirConst()*v*v
 
 accel = 0
-while Height > 0:
+while int(height) > 0:
     time += dT
+
     Air = calcAirRes()
     Grav = calcGrav()
 
     v = v+accel*dT
     accel = (Grav-Air)/mass
-    Height = Height - v*dT
-    print("Height: " , round(Height, 4) ,"   Speed: " , round(v, 4), "   Time: ", round(time, 4), " Gravity: ", round(Grav, 4), " AirRes: ", round(Air, 4))
+    height = height - v*dT
+    timeList.append(time)
+    heightList.append(height)
+    speedList.append(v)
+    airList.append(Air)
+    accelList.append(accel)
+
+
+plt.plot(speedList, airList, color='red', linewidth = .5)
+# plt.title('height at any time')
+plt.xlabel('speed')
+plt.ylabel('air')
+plt.grid(True)
+plt.show()
