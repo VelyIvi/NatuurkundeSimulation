f7b82acdf8fc214db4a24ff6e13d3585275fa8d7
Ivi_ <ivan.velychko.me@gmail.com>
2022-11-11 13:20:21 +0100

    Finnished the simulation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 PI = 3.14159265359
 
-height = 401000.0 #meters
+height = float(401000.0) #meters
 v = 0.0 #m/s
 
 mass = 1.0 #kilos
@@ -24,54 +24,70 @@ speedList = []
 accelList = []
 airList = []
 
-def calcGrav():
-    force = gravityConstant * ((massEarth*mass)/(pow(height+earthRad, 2)))
-    return force
-
 #https://en.wikipedia.org/wiki/Density_of_air
-SLSAPressure = 101325
+SLSAPressure = 101325.0
 SLSTemperature = 288.15
 ESGAcceleration = 9.80665
 LRTemperature = 0.0065
 IGConstant = 8.31446
 MolarMass = 0.0289652
+
+def lerp(A, B, t):
+    print(A + (B-A)*t)
+    return A + (B-A)*t
+def calcGrav():
+    return gravityConstant * ((massEarth*mass)/(pow(height+earthRad, 2)))
+
 def calcAirPressure():
-    press = SLSAPressure*pow((1-(LRTemperature*height)/(SLSTemperature)), ((ESGAcceleration*MolarMass)/(IGConstant*LRTemperature)))
-    return press
+    if(height<=18000):
+        pressure = SLSAPressure*pow((1-(LRTemperature*height)/SLSTemperature), ((ESGAcceleration*MolarMass)/(IGConstant*LRTemperature)))
+        return pressure
+    elif height > 18000 and height < 50000:
+        return 100
+        # return lerp(600, 100, (50000-18000)/(height-18000))
+    elif height > 50000 and height < 85000:
+        return 1
+    elif height > 85000 and height < 690000:
+        return .1
+    elif height > 690000:
+        return 0
+    return 0
 def calcAirDensity():
     press = calcAirPressure()
-    density = (press*MolarMass)/(IGConstant*(SLSTemperature-LRTemperature*height))
-    return density
+    return (press*MolarMass)/(IGConstant*(SLSTemperature-LRTemperature*height))
 
 def calcAirConst():
     density = calcAirDensity()
-    AirConstant = (density*DragCoef*areaMove)/(2)
-    return AirConstant
+    return (density*DragCoef*areaMove)/2
 
 
 def calcAirRes():
-    return calcAirConst()*v*v
+    con = calcAirConst()
+    return con*v*v
 
 accel = 0
-while int(height) > 0:
+
+while height > 0.0:
     time += dT
 
     Air = calcAirRes()
     Grav = calcGrav()
 
-    v = v+accel*dT
+    v += accel*dT
     accel = (Grav-Air)/mass
-    height = height - v*dT
+    height -= v*dT
     timeList.append(time)
+
     heightList.append(height)
     speedList.append(v)
     airList.append(Air)
     accelList.append(accel)
 
 
-plt.plot(speedList, airList, color='red', linewidth = .5)
-# plt.title('height at any time')
-plt.xlabel('speed')
-plt.ylabel('air')
+plt.plot(heightList, airList, color='red', linewidth = .5)
+# plt.gca().invert_xaxis()
+
+plt.xlabel('height')
+plt.ylabel('speed')
 plt.grid(True)
 plt.show()
